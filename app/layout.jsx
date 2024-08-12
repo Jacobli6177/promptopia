7b983ca0eaefb2e631eb310d17d0fc2780d7f51e
Jacobli6177 <101788026+Jacobli6177@users.noreply.github.com>
2024-08-11 22:00:06 -0400

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import '@styles/global.css'
+import { children } from 'react'
+
+export const metadata = {
+    title: "Promptopia",
+    description: 'Discover & Share AI Prompts'
+}
+const Rootlayout = ( { children }) => {
+  return (
+      <html lang='en'>
+        <body>
+            <div className='main'>
+                <div className='gradient'/>
+            </div>
+            <main className='app'>
+                {children}
+            </main>
+        </body>
+      </html>
+  )
+}
+
+export default Rootlayout;
